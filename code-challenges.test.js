814512fdc818adf4e60c65e7a8d1e44b5b271673
jameshlee1 <james.h.lee3@outlook.com>
2023-04-17 08:47:38 -0700

    week-5-assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,18 +14,52 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
+describe('codedMessage', () => {
+    it("converts a to 4, e to 3, i to 1, and o to 0", () => {
+      expect(codedMessage(secretCodeWordOne)).toEqual("L4ck4d41s1c4l");
+      expect(codedMessage(secretCodeWordTwo)).toEqual("G0bbl3dyg00k");
+      expect(codedMessage(secretCodeWordThree)).toEqual("3cc3ntr1c");
+    });
+  });
+
+
+const secretCodeWordOne = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
+const secretCodeWordTwo = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
+const secretCodeWordThree = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+// Pseudo: Create a function called codedMessage that takes in a string as a parameter. I will use the split method to return the string into an array. Then i will use the map method to loop through the array. Then using a conditional statement it will convert the letter to the number.
+
+const codedMessage = (string) => {
+    array = string.split("").map((value) => {
+      if (value.toLowerCase() === "a") return "4";
+      else if (value.toLowerCase() === "e") return "3";
+      else if (value.toLowerCase() === "i") return "1";
+      else if (value.toLowerCase() === "o") return "0";
+      else return value;
+    });
+    return array.join("");
+  };
+
+//   $ /Users/learnacademy/Desktop/week-5-assessment-jameshlee1/node_modules/.bin/jest
+//  PASS  ./code-challenges.test.js
+//   codedMessage
+//     ✓ converts a to 4, e to 3, i to 1, and o to 0 (1 ms)
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+
+describe('newArray', () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter.", () => {
+        expect(newArray(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(newArray(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -35,18 +69,37 @@ const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+// Pseudo: Create a function newArray that takes an array and string as a parameter. I will use the filter method. to filter out any strings in the array that does not have the given letter in either uppercase or lowercase.
+
+const newArray = (array, letter) => {
+    return array.filter((value) => value.includes(letter.toLowerCase()) || value.includes(letter.toUpperCase()))
+}
+
+// PASS  ./code-challenges.test.js
+// newArray
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter. (1 ms)
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+describe('fullHouse', () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        expect(fullHouse(handOne)).toEqual(true)
+        expect(fullHouse(handTwo)).toEqual(false)
+        expect(fullHouse(handThree)).toEqual(false)
+        expect(fullHouse(handFour)).toEqual(true)
+    })
+})
+
+const handOne = [5, 5, 5, 3, 3]
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
+const handTwo = [5, 5, 3, 3, 4]
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+const handThree = [5, 5, 5, 5, 4]
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
+const handFour = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,45 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: Super method allows data to be passed along classes and sub-classes.
 
-Researched answer:
+Researched answer: It calls a method on the parent class with the same name as the method that calls super. For example, if you call a method named i_like_chocolate, and then you call super within that method, Ruby will try to find another method with that same name on the parent class of whoever owns this method.
 
-2. What is a gem?
+2. What is a gem? 
 
-Your answer:
+Your answer: Gems are dependencies and file packages that is used for Ruby.
 
-Researched answer:
+Researched answer: Gem is an open source libraries that contain Ruby code and are packaged with a little extra data . Using a gem allows a programmer to use the code within the gem in their own program, without explicitly inserting that code.
+
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: a relational database uses the SQL and stores data in a table that relates to each other through a primary key.
 
-Researched answer:
+Researched answer: A relational database management system is a program used to create, update, and manage relational databases. Some of the most well-known include MySQL, PostgreSQL, MariaDB, Microsoft SQL Server, and Oracle Database.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: A primary key is the parent container that has values and keys.
 
-Researched answer:
+Researched answer: A primary key contains unique values and identifies each row in a table. For some databases, the primary key cannot contain NULL values. A table can have only one primary key and this primary key can consist of single or multiple columns
 
-5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
+5. What are the HTTP verbs? What is each verb's corresponding CRUD action? 
 
-Your answer:
+Your answer: Create sends data to server, Read retrieve the data from the server, Update modify and existing data from server and Delete removes the data from server.
 
-Researched answer:
+Researched answer: The primary or most-commonly-used HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, PATCH, and DELETE. These correspond to create, read, update, and delete (or CRUD) operations, respectively. There are a number of other verbs, too, but are utilized less frequently.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: Ensure that only the valid information is stored in the database. Rails rus all model validations before information is added and will not save if the validations experiences any errors
 
-2. RESTful routes:
+2. RESTful routes: are how specific requests are directed to code that handles the routes. RESTful routes is a more structured way of defining how a route will work.
 
-3. ERB:
+3. ERB: (Embedded Ruby) is a file that allows the web developer to embed HTML and Ruby code.
 
-4. Params:
+4. Params: add information by passing a value. Adding Params allows the developer to add information by passing the value into the route rather than the URL.
 
-5. API:
+5. API: (Application Programming Interface) which is a set of protocols for building application software.
